4acc196990f4dde7db287cc43506f216c7af4ee0
luis122448 <luis122448@gmail.com>
2025-07-25 00:43:17 -0500

    feture: add close button :/

--- src/app/components/execution-history-modal/execution-history-modal.component.ts
+++ src/app/components/execution-history-modal/execution-history-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { DIALOG_DATA } from '@angular/cdk/dialog';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { ApiService } from '../../services/api.service';
 import { ExecutionReport } from '../../models/api.model';
 import { CommonModule } from '@angular/common';
@@ -13,16 +13,49 @@ import { MiliSecondsConverterPipe } from '../../pipe/mili-seconds-converter.pipe
   styleUrls: ['./execution-history-modal.component.css']
 })
 export class ExecutionHistoryModalComponent implements OnInit{
-  executions: ExecutionReport[] = [];
+  allExecutions: ExecutionReport[] = [];
+  paginatedExecutions: ExecutionReport[] = [];
+  currentPage = 1;
+  pageSize = 20;
   data = inject(DIALOG_DATA);
 
   constructor(
-    private apiService: ApiService
+    private apiService: ApiService,
+    public dialogRef: DialogRef<string>,
   ) { }
 
   ngOnInit(): void {
     this.apiService.getExecutionsByReport(this.data.id_cia, this.data.id_report).subscribe(response => {
-      this.executions = response.list;
+      this.allExecutions = response.list;
+      this.updatePaginatedExecutions();
     });
+  }
+
+  updatePaginatedExecutions(): void {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedExecutions = this.allExecutions.slice(startIndex, endIndex);
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePaginatedExecutions();
+    }
+  }
+
+  nextPage(): void {
+    if ((this.currentPage * this.pageSize) < this.allExecutions.length) {
+      this.currentPage++;
+      this.updatePaginatedExecutions();
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.allExecutions.length / this.pageSize);
+  }
+
+  close(): void {
+    this.dialogRef.close();
   }
 }
